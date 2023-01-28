41a4e3751eb3ce76a359086e2fd03d3e802a32b0
Moyosore Elijah <msontop.me@gmail.com>
2023-01-28 08:49:07 -0800

    ...

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logonbg.png'
 import './Navigations.css'
@@ -8,53 +8,55 @@ import menu from '../assets/menu.png'
 
 const Navigations = () => {
     const [open, setOpen] = useState(false)
-  return (
-    <>
-        <nav class='bg-black hidden md:flex items-center justify-between px-3'>
-            <div>
-                <div class='bg-black flex items-center'>
-                    <img src={logo} width={50} height={50} alt='logo'/>
-                    <p class='text-xl ml-1 font-bold text-white'>miro</p>
+    return (
+        <>
+            <nav class='bg-black hidden md:flex items-center justify-between px-3'>
+                <div>
+                    <Link to='/'>
+                        <div class='bg-black flex items-center'>
+                            <img src={logo} width={50} height={50} alt='logo' />
+                            <p class='text-xl ml-1 font-bold text-white'>miro</p>
+                        </div>
+                    </Link>
                 </div>
-            </div>
-            <ul class='navigations flex gap-8'>
-                <Link to='/crea-un-account' class='flex items-center gap-2'>
-                    <img src={profile} width={40} height={40} alt='profile'/>
-                    <li>crea un account</li>
-                </Link>
-                <Link to='/accedi' class='flex items-center'>
-                    <img src={chat} width={40} height={40} alt='chat'/>
-                    <li>accedi</li>
-                </Link>
-            </ul>
-        </nav>
+                <ul class='navigations flex gap-8'>
+                    <Link to='/crea-un-account' class='flex items-center gap-2'>
+                        <img src={profile} width={40} height={40} alt='profile' />
+                        <li>crea un account</li>
+                    </Link>
+                    <Link to='/accedi' class='flex items-center'>
+                        <img src={chat} width={40} height={40} alt='chat' />
+                        <li>accedi</li>
+                    </Link>
+                </ul>
+            </nav>
 
-        <nav class='bg-[black] z-40 block md:hidden px-3'>
-            <div class='flex justify-between items-center pr-3'>
-                <Link to='/'>
-                <div class='bg-black flex items-center'>
-                    <img src={logo} width={50} height={50} alt='logo'/>
-                    <p class='text-xl ml-1 font-bold text-white'>miro</p>
+            <nav class='bg-[black] z-40 block md:hidden px-3'>
+                <div class='flex justify-between items-center pr-3'>
+                    <Link to='/'>
+                        <div class='bg-black flex items-center'>
+                            <img src={logo} width={50} height={50} alt='logo' />
+                            <p class='text-xl ml-1 font-bold text-white'>miro</p>
+                        </div>
+                    </Link>
+                    <div onClick={() => setOpen(!open)}><img src={menu} width={40} height={40} alt='menu' /></div>
                 </div>
-                </Link>
-                <div onClick={() => setOpen(!open)}><img src={menu} width={40} height={40} alt='menu'/></div>
-            </div>
-            <div class='flex justify-center'>
-                {open && <ul class='navigations flex gap-8 py-2'>
-                <Link to='/crea-un-account' class='flex items-center gap-2' onClick={() => setOpen(false)}>
-                    <img src={profile} width={40} height={40} alt='profile'/>
-                    <li>crea un account</li>
-                </Link>
-                <Link to='/accedi' class='flex items-center' onClick={() => setOpen(false)}>
-                    <img src={chat} width={40} height={40} alt='chat'/>
-                    <li>accedi</li>
-                </Link>
-            </ul>}
-            </div>
-            
-        </nav>
-    </>
-  )
+                <div class='flex justify-center'>
+                    {open && <ul class='navigations flex gap-8 py-2'>
+                        <Link to='/crea-un-account' class='flex items-center gap-2' onClick={() => setOpen(false)}>
+                            <img src={profile} width={40} height={40} alt='profile' />
+                            <li>crea un account</li>
+                        </Link>
+                        <Link to='/accedi' class='flex items-center' onClick={() => setOpen(false)}>
+                            <img src={chat} width={40} height={40} alt='chat' />
+                            <li>accedi</li>
+                        </Link>
+                    </ul>}
+                </div>
+
+            </nav>
+        </>
+    )
 }
 
 export default Navigations
