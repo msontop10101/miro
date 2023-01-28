267e50079bbabc717da3fabbd777905c3af94b24
Moyosore Elijah <msontop.me@gmail.com>
2023-01-28 08:50:57 -0800

    ...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navigations from './components/Navigations';
 import Accedi from './pages/Accedi';
 import CreateAccount from './pages/CreateAccount';
 import LandingPage from './pages/LandingPage';
-import Sitefooter from './components/Footer';
+import Sitefooter from './components/SiteFooter';
 
 
 function App() {
