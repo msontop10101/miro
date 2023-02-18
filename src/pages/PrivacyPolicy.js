537eabfc1cd31b1b9fe3ef8e0c685500de07ed6b
elijah <msontop.me@gmail.com>
2023-02-18 23:09:58 +0100

    ...

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -5,9 +5,38 @@ import Sitefooter from '../components/SiteFooter'
 const PrivacyPolicy = () => {
   return (
     <>
-    <Navigations/>
-    <p>Privacy</p>
-    <Sitefooter/>
+      <Navigations />
+      <div className='bg-[#1c2022] py-4'>
+        <div className='flex h-full text-white w-full justify-center'>
+          <div className='w-[80%]'>
+            <p className='text-2xl font-bold py-2 text-center'>Privacy Policy di Miro</p>
+            <p>
+
+              Miro è un chatbot creato utilizzando le chiavi API di OpenAI. La nostra politica sulla privacy è stata creata per proteggere le informazioni personali dei nostri utenti. La presente privacy policy descrive come vengono raccolti, utilizzati e divulgati i dati personali degli utenti del nostro servizio.
+              <br />
+              Raccolta di informazioni personali
+              Miro richiede ai suoi utenti di fornire informazioni personali come nome e cognome, indirizzo e-mail e password durante la fase di registrazione. Queste informazioni sono necessarie per fornire un servizio personalizzato e per garantire l'accesso sicuro al nostro servizio.
+              <br />
+              Uso delle informazioni personali
+              Le informazioni personali fornite dagli utenti vengono utilizzate solo per fornire il servizio richiesto e per comunicare con gli utenti riguardo al servizio. Non vengono condivise con terze parti, a meno che non sia richiesto dalla legge.
+              <br />
+              Conservazione delle informazioni personali
+              Le informazioni personali degli utenti vengono conservate nel nostro sistema finché l'utente non richiede la cancellazione del proprio account. In caso di cancellazione dell'account, le informazioni personali verranno cancellate dal nostro sistema.
+              <br />
+              Sicurezza delle informazioni personali
+              Miro adotta misure di sicurezza tecniche e organizzative per proteggere le informazioni personali degli utenti da accessi non autorizzati, modifiche o divulgazioni.
+              <br />
+              Modifiche alla privacy policy
+              Miro si riserva il diritto di modificare la presente privacy policy in qualsiasi momento. Tutte le modifiche saranno comunicate agli utenti tramite e-mail o attraverso il nostro servizio.
+              <br />
+              Contatti
+              Per qualsiasi domanda o commento riguardo alla nostra privacy policy o alle informazioni personali degli utenti, si prega di contattare il nostro servizio clienti tramite e-mail all'indirizzo [inserire qui l'indirizzo e-mail del servizio clienti].
+              <br />
+              Ultimo aggiornamento: [inserire la data dell'ultimo aggiornamento della privacy policy].</p>
+          </div>
+        </div>
+      </div>
+      <Sitefooter />
     </>
   )
 }
