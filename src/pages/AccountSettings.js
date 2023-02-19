10cf21dd01c5d3ff7f8479a5f74dfa33f47f0fde
elijah <msontop.me@gmail.com>
2023-02-19 01:56:48 +0100

    edited

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -10,20 +10,19 @@ const AccountSettings = () => {
   return (
     <>
     <Navigations/>
-      <div class='w-full overflow-x-hidden bg-black px-3 flex' style={{height: 'calc(100vh - 68px)'}}>
+      <div class='w-full overflow-hidden bg-black px-3 flex' style={{height: 'calc(100vh - 68px)'}}>
         <div class='flex z-40 justify-center flex-col w-full relative items-center'>
         <h1 class='text-white text-4xl header-font font-bold mb-2 leading-[4rem] z-30'>Impostazioni account</h1>
         <AccountSettingsForm/>
-        
-        </div>
-        <div>
-        <div class='absolute top-5 md:hidden top-[-20%] left-[-30%]'>
+        <div class='absolute top-5 md:hidden top-[-30%] left-[-30%]'>
           <img src={two} alt='two'/>
         </div>
-        <div class='absolute bottom-0 md:hidden top-30 right-[-45%]'>
+        <div class='absolute bottom-0 md:hidden top-20 right-[-45%]'>
           <img src={three} alt='three'/>
         </div>
         </div>
+        
+        
       </div>
       <Sitefooter/>
     </>
