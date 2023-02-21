2f45c4711829843b30dc47e9f8805d43e0d22c94
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-02-21 11:03:41 +0100

    reviewed signup

--- src/context/auth/signup.jsx
+++ src/context/auth/signup.jsx
@@ -4,48 +4,46 @@ import { useState, useContext } from "react";
 import { signUp } from "./comp/signup";
 
 export const SignUpContext = React.createContext({
-    error: null,
-    success: false,
-    loading: false,
-    signup: (value) => {}
+  error: null,
+  success: false,
+  loading: false,
+  signup: (value) => {},
 });
 
+export const SignUpProvider = ({ children }) => {
+  const [success, setsucess] = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setIsloading] = useState(false);
 
-
-export const SignUpProvider = ( { children }) => {
-    const [success, setsucess] = useState(false);
-    const [error, setError] = useState("");
-    const [loading, setIsloading] = useState(false)
-
-    const Signup = (data) => {
-        console.log("signing up 2")
-        setIsloading(true)
-        signUp(data)
-        .then((res) => {
-            console.log(res)
-            setIsloading(false)
-            setsucess(true);
-            setError(null);
-        })
-        .catch((err) => {
-          console.log(err)
-            setIsloading(false)
-            setsucess(false);
-            setError(err.response);
-        })
-    }
-    return (
-      <SignUpContext.Provider
-        value={{
-          error,
-          success,
-          loading,
-          signup: Signup
-        }}
-      >
-        {children}
-      </SignUpContext.Provider>
-    );
+  const Signup = (data) => {
+    console.log("signing up 2");
+    setIsloading(true);
+    signUp(data)
+      .then((res) => {
+        console.log(res);
+        setIsloading(false);
+        setsucess(true);
+        setError(null);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsloading(false);
+        setsucess(false);
+        setError(err.response);
+      });
   };
+  return (
+    <SignUpContext.Provider
+      value={{
+        error,
+        success,
+        loading,
+        signup: Signup,
+      }}
+    >
+      {children}
+    </SignUpContext.Provider>
+  );
+};
 
-  export const useSignupContext = () => useContext(SignUpContext);
+export const useSignupContext = () => useContext(SignUpContext);
